10f73bb1c30c4ed3b63ac20a46d94299a23b5e0e
Vinicius Guedes LIrio <guedes676@gmail.com>
2018-01-27 10:31:27 -0200

    add @Before

--- prova/src/test/java/br/com/prova/repository/CampanhaRepositoryTests.java
+++ prova/src/test/java/br/com/prova/repository/CampanhaRepositoryTests.java
@@ -1,15 +1,17 @@
 package br.com.prova.repository;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.assertj.core.api.Assertions.*;
 
 import br.com.prova.model.Campanha;
 import br.com.prova.model.TimeCoracao;
@@ -23,16 +25,18 @@ public class CampanhaRepositoryTests {
 
 	@Autowired
 	private CampanhaRepository repository;
-
-	@Test
-	public void buscarPorVigenciasAtivasTest() throws Exception {
-		
+	
+	@Before
+    public void dataLoader() throws ParseException {
 		this.entityManager.persist(new Campanha(null, "CorinthiansTest", new TimeCoracao("Corinthians"), new SimpleDateFormat("yyyy-MM-dd").parse("2014-02-14"),
 				new SimpleDateFormat("yyyy-MM-dd").parse("2019-10-01")));
 		
-		this.entityManager.persist(new Campanha(null, "BambiTest", new TimeCoracao("Sao Paulo"), new SimpleDateFormat("yyyy-MM-dd").parse("2014-02-14"),
+		this.entityManager.persist(new Campanha(null, "CorinthiansTest2", new TimeCoracao("Meirinhas"), new SimpleDateFormat("yyyy-MM-dd").parse("2014-02-14"),
 				new SimpleDateFormat("yyyy-MM-dd").parse("2019-10-01")));
-		
+    }
+	
+	@Test
+	public void buscarPorVigenciasAtivasTest() throws Exception {
 		
 		List<Campanha> campanhasAtivas = this.repository.buscarPorVigenciasAtivas(new SimpleDateFormat("yyyy-MM-dd").parse("2019-09-01"));
 		
